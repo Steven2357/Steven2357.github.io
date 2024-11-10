f12be2eeffe1fd971bbc6bc5184908ed9b39f95b
Steven2357 <abcd1234dd51@gmail.com>
2024-11-10 21:47:37 +0800

    add Q33-45

--- bridge/Questions/scripts/main.js
+++ bridge/Questions/scripts/main.js
@@ -7,3 +7,8 @@ function Correct(){
     ans=document.getElementById("answer");
     ans.innerHTML="答對了 ^ ^  Correct ^ ^";
 }
+
+function Semicorrect(){
+    ans=document.getElementById("answer");
+    ans.innerHTML="ooooooooooooo 不能說你錯啦...";
+}
